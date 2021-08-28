2211cfc1965ae4a3ddb324551d76769efce066e4
DasRegal <plexus.9310@gmail.com>
2021-08-29 01:57:34 +0700

    Mark regs

--- reg.h
+++ reg.h
@@ -6,9 +6,9 @@
 #define REG_R_MAX       126
 enum
 {
-  E_SHREDDER_ON_OFF_1 = 0,      // 0
-  E_SHREDDER_ON_OFF_2,          // 1
-  E_WEIGHT_1,                   // 2
+  E_SHREDDER_ON_OFF_1 = 0,      // 0 *
+  E_SHREDDER_ON_OFF_2,          // 1 *
+  E_WEIGHT_1,                   // 2 
   E_WEIGHT_2,                   // 3
   E_BLEND_SYSTEM_ON_OFF,        // 4
   E_ACTUATOR_H_IN_OUT,          // 5
@@ -31,39 +31,39 @@ enum
   E_ACTUATOR_V_TIME_8,          // 22
   E_ACTUATOR_V_DELAY_8,         // 23
   E_ACTUATOR_V_SPEED_9,         // 24
-  E_MIXER_ON_OFF,               // 25
-  E_PUMP_ON_OFF,                // 26
-  E_FLUSH_TIME,                 // 27
-  E_DISPENSER_DRY_ON_OFF,       // 28
-  E_DISPENSER_DRY_COUNT,        // 29
+  E_MIXER_ON_OFF,               // 25 *
+  E_PUMP_ON_OFF,                // 26 *
+  E_FLUSH_TIME,                 // 27 *
+  E_DISPENSER_DRY_ON_OFF,       // 28 *
+  E_DISPENSER_DRY_COUNT,        // 29 *
   E_NU_1,                       // 30
-  E_DISPENSER_LIQ_ON_OFF,       // 31
-  E_DISPENSER_LIQ_TIME,         // 32
+  E_DISPENSER_LIQ_ON_OFF,       // 31 *
+  E_DISPENSER_LIQ_TIME,         // 32 *
   E_NU_2,                       // 33
-  E_SERVO_PWM,                  // 34
+  E_SERVO_PWM,                  // 34 *
   E_ACTUATOR_V_DELAY_1,         // 35
   E_ACTUATOR_V_DELAY_2,         // 36
   E_ACTUATOR_V_DELAY_3,         // 37
   E_INIT_SYSTEM_ON_OFF,         // 38
   E_MIXER_DELAY_START,          // 39
-  E_EMERGENCY_STOP,             // 40
-  E_HEARTBIT,                   // 41
-  E_HEARTBIT_START_STOP,        // 42
-  E_HEARTBIT_FREQ,              // 43
+  E_EMERGENCY_STOP,             // 40 *
+  E_HEARTBIT,                   // 41 *
+  E_HEARTBIT_START_STOP,        // 42 *
+  E_HEARTBIT_FREQ,              // 43 *
 
-  E_ACTUATOR_V_SPEED,           // 44
-  E_ACTUATOR_V_TIME,            // 45
-  E_ACTUATOR_V_UP_TOP,          // 46
-  E_ACTUATOR_V_DOWN_MID,        // 47
-  E_ACTUATOR_V_DOWN_BOT,        // 48
+  E_ACTUATOR_V_SPEED,           // 44 *
+  E_ACTUATOR_V_TIME,            // 45 *
+  E_ACTUATOR_V_UP_TOP,          // 46 *
+  E_ACTUATOR_V_DOWN_MID,        // 47 *
+  E_ACTUATOR_V_DOWN_BOT,        // 48 *
 
-  E_ACTUATOR_H_SPEED,           // 49
-  E_ACTUATOR_H_TIME,            // 50
-  E_ACTUATOR_H_IN,              // 51
-  E_ACTUATOR_H_OUT,             // 52
+  E_ACTUATOR_H_SPEED,           // 49 *
+  E_ACTUATOR_H_TIME,            // 50 *
+  E_ACTUATOR_H_IN,              // 51 *
+  E_ACTUATOR_H_OUT,             // 52 *
 
-  E_VERSION = (REG_R_MAX - 2),  // 124
-  E_SUBVERSION = (REG_R_MAX - 1)// 125
+  E_VERSION = (REG_R_MAX - 2),  // 124 *
+  E_SUBVERSION = (REG_R_MAX - 1)// 125 *
 };
 
 int RegInit(void);
